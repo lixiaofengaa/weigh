596211f6ebb68bffd0f19ee4251c9950abe2f6d5
lixiaofeng <lixiaofeng@baoshunchem.com>
2016-08-08 09:40:37 +0800

    modified:   models/dispatche_vehicle.py 	modified:   models/nc_order_line.py

--- models/dispatche_vehicle.py
+++ models/dispatche_vehicle.py
@@ -8,13 +8,13 @@ class dispatche_vehicle_line(models.Model):
     _name = 'weigh.dispatche_vehicle'
     _inherit = ['mail.thread']
 
-    no = fields.Char()
+    no = fields.Char()#派车单号
     state = fields.Selection([('draft', 'Draft'),
                               ('confirmed', 'Confirmed'),
                               ('done', 'Done')],
                              string='Status', index=True, readonly=True, default='draft',
-                             track_visibility='onchange', copy=False)
-    nc_order_line_ids = fields.Many2many('weigh.nc_order.line')
-    fleet_vehicle_ids = fields.Many2one('fleet.vehicle')
+                             track_visibility='onchange', copy=False)#状态
+    nc_order_line_ids = fields.Many2many('weigh.nc_order.line')#NC订单号
+    fleet_vehicle_ids = fields.Many2one('fleet.vehicle')#车队号
 
-    estimate_vehicle_entry = fields.Datetime()
+    estimate_vehicle_entry = fields.Datetime()#估计车辆进入时间
--- models/nc_order_line.py
+++ models/nc_order_line.py
@@ -3,25 +3,24 @@ from openerp import models, fields, api
 from openerp.osv import osv
 from openerp.tools.translate import _
 
-
 class nc_order_line(models.Model):
     _name = 'weigh.nc_order.line'
     _inherit = ['mail.thread']
 
     nc_order_no = fields.Char()#nc订单号
-    consignee = fields.Char()#
-    consignor = fields.Char()
-    place = fields.Char()
-    price = fields.Float()
-    num = fields.Float()
-    order_type = fields.Char()
-    nc_maker = fields.Char()
-    product_template_id = fields.Many2one('product.template')
+    consignee = fields.Char()#收货人
+    consignor = fields.Char()#发货人
+    place = fields.Char()#地点
+    price = fields.Float()#金额
+    num = fields.Float()#数量
+    order_type = fields.Char()#订单类型
+    nc_maker = fields.Char()#制单人
+    product_template_id = fields.Many2one('product.template')#模板
     state = fields.Selection([('draft', 'Draft'),
                               ('confirmed', 'Confirmed'),
                               ('done', 'Done')],
                              string='Status', index=True, readonly=True, default='draft',
-                             track_visibility='onchange', copy=False)
+                             track_visibility='onchange', copy=False)#状态
     @api.one
     def name_get(self):
         return self.id, self.nc_order_no
